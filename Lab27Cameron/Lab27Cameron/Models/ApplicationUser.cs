26edf4415a58f15e00b1b07bbc08c18ac9d4489e
Cameron Pam <cammpam.creative@gmail.com>
2017-10-18 15:03:20 -0700

    MVC is setu[

---
+++ Lab27Cameron/Lab27Cameron/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab27Cameron.Models
+{
+    public class ApplicationUser
+    {
+        public int ApplicationUserID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Skillset 1")]
+        public string Role { get; set; }
+
+        [Display(Name = "Skillset 2")]
+        public string Role2 { get; set; }
+    }
+}
---
+++ Lab27Cameron/Lab27Cameron/Models/Song.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab27Cameron.Models
+{
+    public class Song
+    {
+        [Key]
+        public int SongID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
+        [Display(Name = "Song Credit")]
+        public string Contributor1Role { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Contributor1 { get; set; }
+        [Display(Name = "Song Credit")]
+        public string Contributor2Role { get; set; }
+        [Display(Name = "Name")]
+        public string Contributor2 { get; set; }
+        [Display(Name = "Song Credit")]
+        public string Contributor3Role { get; set; }
+        [Display(Name = "Name")]
+        public string Contributor3 { get; set; }
+    }
+}
